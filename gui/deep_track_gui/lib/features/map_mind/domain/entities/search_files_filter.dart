2aa8d8665d866b663190a86c0a74513ed4799e8d
LucasMatheusDev <lucasmatheusdev@gmail.com>
2024-10-17 18:38:33 -0300

    Refatorar método não utilizado e remover import desnecessário
    O método `getFiles` foi removido da classe `SearchFilesFilter`
    por não ser utilizado no código, e o import `dart:io` foi removido.

--- gui/deep_track_gui/lib/features/map_mind/domain/entities/search_files_filter.dart
+++ gui/deep_track_gui/lib/features/map_mind/domain/entities/search_files_filter.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 class SearchFilesFilter {
   final String basePath;
   final List<Pattern> patternsFiles;
@@ -10,18 +8,4 @@ class SearchFilesFilter {
     required this.patternsFiles,
     this.imporPattern,
   });
-
-  Future<List<FileSystemEntity>> getFiles(Directory directory) async {
-    final dartFiles = <FileSystemEntity>[];
-
-    await for (var entity in directory.list(recursive: true)) {
-      if (entity is File &&
-          patternsFiles
-              .any((pattern) => pattern.allMatches(entity.path).isNotEmpty)) {
-        dartFiles.add(entity);
-      }
-    }
-
-    return dartFiles;
-  }
 }
