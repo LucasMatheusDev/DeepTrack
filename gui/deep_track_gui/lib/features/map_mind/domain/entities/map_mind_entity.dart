0c3bffd372f65e386f31e83da0a056bd73414ecf
LucasMatheusDev <lucasmatheusdev@gmail.com>
2024-12-09 21:09:20 -0300

    Add layer filtering functionality to FilesAnalyzerInfo
    This commit adds the ability to filter layers based on a given condition in the FilesAnalyzerInfo class. The `layerByFilter` method now accepts an optional `filter` parameter and returns a filtered list of layers based on that condition. This allows for more granular control over the layers that are returned. This change enhances the flexibility and extensibility of the FilesAnalyzerInfo class.

--- gui/deep_track_gui/lib/features/map_mind/domain/entities/map_mind_entity.dart
+++ gui/deep_track_gui/lib/features/map_mind/domain/entities/map_mind_entity.dart
@@ -1,11 +1,16 @@
+import 'package:deep_track_cli/commands/analyzer_imports_command.dart';
 import 'package:deep_track_gui/features/map_mind/domain/entities/file_map_analyzer.dart';
 
 class FilesAnalyzerInfo {
   final List<FileMapMindAnalyzer> fileAnalyzer;
+  final List<Layer> layers;
 
-  FilesAnalyzerInfo(this.fileAnalyzer);
+  FilesAnalyzerInfo(this.fileAnalyzer, {this.layers = const []});
 
   bool Function(FileMapMindAnalyzer) _ruleFilter = (file) => true;
+
+  bool Function(FileMapMindAnalyzer) get ruleFilter => _ruleFilter;
+
   bool updateRule(bool Function(FileMapMindAnalyzer) filter) {
     _ruleFilter = filter;
     return true;
@@ -18,6 +23,27 @@ class FilesAnalyzerInfo {
       updateRule(filter);
     }
     return fileAnalyzer.where(_ruleFilter).toList();
+  }
+
+  List<Layer> layerByFilter({
+    bool Function(Layer)? filter,
+  }) {
+    if (filter != null) {
+      return layers.where(filter).toList();
+    } else {
+      return layers.where((layer) {
+        final files = layer.files;
+
+        return files.any((file) => _ruleFilter(
+              FileMapMindAnalyzer(
+                nameFile: file.nameFile,
+                imports: file.imports,
+                path: file.path,
+                references: file.references,
+              ),
+            ));
+      }).toList();
+    }
   }
 
   List<FileMapMindAnalyzer> withoutReferences() {
